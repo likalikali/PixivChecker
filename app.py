1c928ffe326f771e5eb177e637103bc2d9935a8b
likalikali <ray.zhang.ruiji@foxmail.com>
2026-02-06 23:53:14 +0800

    second commit

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from sender import check_pixiv  # 你的主函数
 
 app = Flask(__name__)
 
-@app.route('/trigger')
+@app.route('/trigger', methods=['GET'])
 def trigger():
     check_pixiv()
-    return "Executed successfully", 200
+    return "Executed", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run()  # 本地测试
